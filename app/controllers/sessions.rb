f69cc6994634030928bd0890b1e26e03936d79a2
BrunoBoehm <bruno.boehm@gmail.com>
2016-12-23 20:57:03 +0100

    added the login logout functionnality with bcrypt

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
 		erb :"sessions/login.html"
 	end
 
+	post "/login" do
+		user = User.find_by(name: params[:name], surname: params[:surname])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/users"
+		else
+			erb :"sessions/login.html"
+		end
+	end	
+
 	get "/logout" do
 		session.clear
 		redirect "/lists"
