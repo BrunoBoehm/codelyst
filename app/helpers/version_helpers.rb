d7f065000ad768522938433f8998daf0600a5532
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-31 17:53:44 +0100

    improved design of show list and index snippets

--- app/helpers/version_helpers.rb
+++ app/helpers/version_helpers.rb
@@ -8,6 +8,10 @@ module Sinatra
 		def full_url(link)
 			link.scan(/https?:\/\/[\S]+/i)[0].gsub(/https?:\/\//, "").split('"').first
 		end
+
+		def truncate(text, max = 100)
+			text.length > max ? "#{text[0...max]}..." : text
+		end
 	end
 
 
